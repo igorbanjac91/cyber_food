a2fc711ef03b4a48fbc2b39e682a3a179332eac8
IgorBanjac91 <igor.banjac91@gmail.com>
2021-06-28 09:46:54 +0200

    add name and descrption presence validation for food items

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,4 +1,6 @@
 class FoodItem < ApplicationRecord
 
+  validates :name, :description, presence: true
+
   has_many :order_items
 end
--- spec/models/food_item_spec.rb
+++ spec/models/food_item_spec.rb
@@ -7,4 +7,14 @@ RSpec.describe FoodItem, type: :model do
   it "is valid" do 
     expect(food_item).to be_valid
   end
+
+  it "is invalid without a name"  do 
+    food_item.name = ""
+    expect(food_item).to_not be_valid
+  end
+
+  it "is invalid without a description"  do 
+    food_item.description = ""
+    expect(food_item).to_not be_valid
+  end
 end
