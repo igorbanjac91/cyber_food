6df47da6ef79748c2140fec56b0b574b0c31e5c9
IgorBanjac91 <igor.banjac91@gmail.com>
2021-08-06 14:51:46 +0200

    play with stripe api, session hijack

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -3,15 +3,16 @@ class CheckoutController < ApplicationController
 
   def create
     @order = Order.find(params[:order_id])
+    @food_item = @order.order_items[0].food_item
     @session = Stripe::Checkout::Session.create({
-      customer: current_user.stripe_customer_id
+      customer: current_user.stripe_customer_id,
       payment_method_types: ['card'],
       line_items: [{
-        price: product.stripe_price_id,
-        quantity: 1,
+        price: @food_item.stripe_price_id,
+        quantity: 1
       }],
       mode: 'payment',
-      success_url: root_url,
+      success_url: root_url + "?session_id={CHECKOUT_SESSION_ID}",
       cancel_url: root_url,
     })
     respond_to do |format|  
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.create(
+User.find_or_create_by(
   first_name: "admin",
   last_name: "admin",
   email: "admin@gmail.com",
@@ -6,7 +6,8 @@ User.create(
   password_confirmation: "password",
   admin: true
 )
-User.create(
+
+User.find_or_create_by(
   first_name: "John",
   last_name: "Smith",
   email: "user@gmail.com",
@@ -15,7 +16,7 @@ User.create(
 )
 
 3.times do |n|
-  User.create(
+  User.find_or_create_by(
     first_name: "name-#{n}",
     last_name: "last_name-#{n}",
     email: "user-#{n}@gmail.com",
@@ -24,47 +25,73 @@ User.create(
   )
 end
 
-pizza_category = Category.create(name: "pizza")
-pizza_category.image.attach(io: File.open('app/assets/images/database_seed/pizzas.jpg'), filename: 'pizzas.jpg')
-pasta_category = Category.create(name: "pasta")
-pasta_category.image.attach(io: File.open('app/assets/images/database_seed/pastas.jpg'), filename: 'pastas.jpg')
-mains_category = Category.create(name: "mains")
-mains_category.image.attach(io: File.open('app/assets/images/database_seed/main.jpg'), filename: 'main.jpg')
-antipasti_category = Category.create(name: "antipasti")
-antipasti_category.image.attach(io: File.open('app/assets/images/database_seed/antipasti.jpg'), filename: 'antipasti.jpg')
-drinks_category = Category.create(name: "drinks")
-drinks_category.image.attach(io: File.open('app/assets/images/database_seed/drinks.jpg'), filename: 'drinks.jpg')
+# Create Categories
 
-penne_pomodoro = FoodItem.create(name: "Penne Pomodoro",
+pizza_category = Category.find_or_create_by(name: "pizza")
+pasta_category = Category.find_or_create_by(name: "pasta")
+mains_category = Category.find_or_create_by(name: "mains")
+antipasti_category = Category.find_or_create_by(name: "antipasti")
+drinks_category = Category.find_or_create_by(name: "drinks")
+
+# Create Food Items
+
+penne_pomodoro = FoodItem.find_or_create_by(name: "Penne Pomodoro",
                 description: "Sweet tomato sauce, garlic, basil, mascarpone, mozzarella,Italian hard cheese & herby breadcrumbs", 
                 category: pasta_category,
                 price: 1220)               
-penne_pomodoro.image.attach(io: File.open('app/assets/images/database_seed/penne-pomodoro.jpg'), filename: 'penne-pomodoro.jpg')
 
-pizza_julietta = FoodItem.create(name: "Julietta",
+pizza_julietta = FoodItem.find_or_create_by(name: "Julietta",
                 description: "Slow-roasted tomatoes, basil, the best buffalo mozzarella & aged Parmesan", 
                 category: pizza_category,
                 price: 1280)
-pizza_julietta.image.attach(io: File.open('app/assets/images/database_seed/pizza-julietta.jpg'), filename: 'pizza-julietta.jpg')
 
-plank = FoodItem.create(name: "Seasonal Vegetables Plank", 
+plank = FoodItem.find_or_create_by(name: "Seasonal Vegetables Plank", 
                 description: "Marinated & grilled pepper, courgette & aubergine with baby mozzarella, focaccia, grissini, olives & pickles",
                 category: antipasti_category,
                 price: 700)
-plank.image.attach(io: File.open('app/assets/images/database_seed/plank.jpg'), filename: 'plank.jpg')
                 
 
-pork = FoodItem.create(name: "Pork Chop Panzanella",
+pork = FoodItem.find_or_create_by(name: "Pork Chop Panzanella",
                 description: "With smashed ricotta & feta, cherry tomato panzanella salad & fresh oregano, served with spiced fries", 
                 category: mains_category,
                 price: 1380)      
-pork.image.attach(io: File.open('app/assets/images/database_seed/pork-chop.jpg'), filename: 'pork-chop.jpg')                
 
 
-coco_cola = FoodItem.create(name: "Coca Cola",
+coco_cola = FoodItem.find_or_create_by(name: "Coca Cola",
                 description: "Origianal Coca Cola", 
                 category: drinks_category,
                 price: 250)                
-coco_cola.image.attach(io: File.open('app/assets/images/database_seed/coca-cola.jpg'), filename: 'coca-cola.jpg')
 
 
+# Attach images 
+
+attach_image(pizza_category, 'app/assets/images/database_seed/pizzas.jpg', 'pizzas.jpg')
+attach_image(pasta_category, 'app/assets/images/database_seed/pastas.jpg', 'pastas.jpg')
+attach_image(mains_category, 'app/assets/images/database_seed/main', 'main.jpg')
+attach_image(drinks_category, 'app/assets/images/database_seed/drinks.jpg', 'drinks.jpg')
+
+attach_image(penne_pomodoro, 'app/assets/images/database_seed/penne-pomodoro.jpg', 'penne-pomodoro.jpg')
+attach_image(pizza_julietta, 'app/assets/images/database_seed/pizza-julietta.jpg', 'pizza-julietta.jpg')
+attach_image(plank, 'app/assets/images/database_seed/plank.jpg', 'plank.jpg')
+attach_image(pork, 'app/assets/images/database_seed/pork-chop.jpg', 'pork-chop.jpg')
+attach_image(coco_cola, 'app/assets/images/database_seed/coca-cola.jpg', 'coca-cola.jpg')
+
+
+
+# pizza_category.image.attach(io: File.open('app/assets/images/database_seed/pizzas.jpg'), filename: 'pizzas.jpg')
+# pasta_category.image.attach(io: File.open('app/assets/images/database_seed/pastas.jpg'), filename: 'pastas.jpg')
+# mains_category.image.attach(io: File.open('app/assets/images/database_seed/main.jpg'), filename: 'main.jpg')
+# drinks_category.image.attach(io: File.open('app/assets/images/database_seed/drinks.jpg'), filename: 'drinks.jpg')
+# antipasti_category.image.attach(io: File.open('app/assets/images/database_seed/antipasti.jpg'), filename: 'antipasti.jpg')
+
+# penne_pomodoro.image.attach(io: File.open('app/assets/images/database_seed/penne-pomodoro.jpg'), filename: 'penne-pomodoro.jpg')
+# pizza_julietta.image.attach(io: File.open('app/assets/images/database_seed/pizza-julietta.jpg'), filename: 'pizza-julietta.jpg')
+# plank.image.attach(io: File.open('app/assets/images/database_seed/plank.jpg'), filename: 'plank.jpg')
+# pork.image.attach(io: File.open('app/assets/images/database_seed/pork-chop.jpg'), filename: 'pork-chop.jpg')                
+# coco_cola.image.attach(io: File.open('app/assets/images/database_seed/coca-cola.jpg'), filename: 'coca-cola.jpg')
+
+def attach_image(object, path, filename) 
+  unless objec.image.attached?
+    object.image.attach(io: File.open(path), filename: filename)
+  end
+end
