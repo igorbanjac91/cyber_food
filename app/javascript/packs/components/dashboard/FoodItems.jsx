ad6232b6b55446c93648a79ecfb47250957a07c5
IgorBanjac91 <igor.banjac91@gmail.com>
2021-08-11 10:07:06 +0200

    finish styling dashboard food items page

--- app/javascript/packs/components/dashboard/FoodItems.jsx
+++ app/javascript/packs/components/dashboard/FoodItems.jsx
@@ -1,6 +1,9 @@
 import axios from "axios"
 import setAxiosHeaders from "../AxiosHeaders"
 import React, { useEffect, useState } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPen } from '@fortawesome/free-solid-svg-icons'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 const FoodItems = () => {
 
@@ -8,6 +11,7 @@ const FoodItems = () => {
   const [ name, setName] = useState("");
   const [ description, setDescription ] = useState("");
   const [ price, setPrice ] = useState("");
+  const [ image, setImage ] = useState("");
 
 
   useEffect(() => {
@@ -37,15 +41,23 @@ const FoodItems = () => {
     setPrice(e.target.value)
   }
 
+  function handleImageChange(e) {
+    setImage(e.target.files[0])
+  }
+
   function handleSubmit(e) {
     e.preventDefault()
     setAxiosHeaders()
     
+    const formData = new FormData();
+    formData.append("file", image)
+
     const params = {
       food_item: {
         name: name,
         description: description,
-        price: price
+        price: price,
+        image: formData
       }
     }
 
@@ -76,9 +88,12 @@ const FoodItems = () => {
           <textarea name="food_item[description]" onChange={handleDescriptionChange} placeholder="Description" />
         </div>
         <div className="field">
-          <input type="text" name="food_item[price]"onChange={handlePriceChange} placeholder="price"/>
+          <input type="text" name="food_item[price]"onChange={handlePriceChange} placeholder="Price"/>
         </div>
-        <div className="action">
+        <div className="field">
+          <input type="file" name="food_item[image]" onChange={handleImageChange} />
+        </div>
+        <div className="actions ">
           <button className="submit-btn add-food-items-btn">Add Food Item</button>
         </div>
       </form>
@@ -101,8 +116,17 @@ const FoodItemsListItem = (props) => {
       <p className="description">{foodItem.description}</p>
       <p className="price">{foodItem.price}</p>
       <div className="actions">
-        <button className="edit-btn"></button>  
-        <button className="delete-btn"></button>
+        <button className="edit-btn" onClick={handleEdit}>
+          <FontAwesomeIcon icon={faPen} />
+        </button>  
+        <button className="delete-btn">
+          <FontAwesomeIcon icon={faTrashAlt} onClick={handleDelete} />
+        </button>
+      </div>
+      <div className="food-item__image-container"
+           style={{
+             backgroundImage: `url(${foodItem.image_url})`
+            }}>
       </div>
     </li>
   )
